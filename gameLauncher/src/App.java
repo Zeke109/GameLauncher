f1413368d3a5fa8ac8ec87822a511c4949d77a4a
Zeke109 <zachyvernon@gmail.com>
2025-07-13 13:34:33 -0400

    Added game addition functionality

--- gameLauncher/src/App.java
+++ gameLauncher/src/App.java
@@ -24,7 +24,7 @@ public class App {
                 if (file.createNewFile())
 
                 if (file.length() == 0){
-                    addGames();
+                    addGamesMenu(gameList, null);
                 }
 
                 data = new FileInputStream("src/Data/gameList.csv");
@@ -38,7 +38,7 @@ public class App {
                     if (isFirstLine)
                         isFirstLine = false;
 
-                    String[] part = line.split(",");
+                    String[] part = line.split(", ");
                     ArrayList<String> parts = new ArrayList<String>();
                     for (int i = 0; i < part.length; i++){
                         parts.add(part[i]);
@@ -90,41 +90,32 @@ public class App {
             }
         }
 
-        mainMenu(gameList);
+
+        mainMenu(gameList, null);
         
 
         data.close();
     }
 
-    public static void mainMenu(ArrayList<Games> gameList){
+    public static void mainMenu(ArrayList<Games> gameList, JFrame previous){
         JFrame uiWindow = new JFrame("Zeke's Game Launcher");
         uiWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         uiWindow.setSize(950, 540);
 
-        JPanel homePanel = new JPanel();
-        homePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        homePanel.setBackground(Color.darkGray);
-
-        homePanel.add(Box.createRigidArea(new DimensionUIResource(10, 20)));
-
-        homePanel.add(new JButton("Add New Games"));
-
+        JPanel homePanel = createHomePanel(gameList, uiWindow);
 
         JPanel gamesPanel = new JPanel();
         gamesPanel.setLayout(new BoxLayout(gamesPanel, BoxLayout.Y_AXIS));
         
         gamesPanel.setBackground(new Color(000051204)); // Blue 
 
-
+        System.out.println(gameList.size());
         for (int i = 0; i < gameList.size(); i++){
             
             ImageIcon gameLogo = new ImageIcon(gameList.get(i).getIconPath());
             Image scaledImage = gameLogo.getImage().getScaledInstance(125, 125, Image.SCALE_SMOOTH);
             ImageIcon scaledIcon = new ImageIcon(scaledImage);
             JButton icon = new JButton(gameList.get(i).getName(), scaledIcon);
-
-            System.out.println("Path: " + gameList.get(i).getIconPath());
-            System.out.println("File exists: " + new File(gameList.get(i).getIconPath()).exists());
 
             icon.setPreferredSize(new DimensionUIResource(125, 125));
             icon.setMaximumSize(new DimensionUIResource(125, 125));
@@ -164,7 +155,110 @@ public class App {
         }
     }
 
-    public static void addGames() {
-        System.out.println("Success");
+    public static void addGamesMenu(ArrayList<Games> gameList, JFrame previous) {
+
+        JFrame uiWindow = new JFrame("Zeke's Game Launcher");
+        uiWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        uiWindow.setSize(950, 540);
+
+        JPanel homePanel = createHomePanel(gameList, uiWindow);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+
+        JLabel gameLabel1 = new JLabel("\nEnter the game's name: ");
+        JTextField gameField1 = new JTextField(5); 
+
+        JLabel gameLabel2 = new JLabel("\nEnter the game's path");
+        JTextField gameField2 = new JTextField(1); 
+
+        JLabel gameLabel3 = new JLabel("\nEnter the path for the game icon");
+        JTextField gameField3 = new JTextField(1); 
+
+        JLabel gameLabel4 = new JLabel("\nEnter how many hours you have ");
+        JTextField gameField4 = new JTextField(1); 
+
+        JButton submitButton = new JButton("\nAdd Game");
+
+        submitButton.addActionListener(e -> {
+            Games newGame = new Games();
+            newGame.setName(gameField1.getText());
+            gameField1.setText("");
+            newGame.setFilePath(gameField2.getText());
+            gameField2.setText("");
+            newGame.setIconPath(gameField3.getText());
+            gameField3.setText("");
+            while (true){
+                try{
+                    newGame.setGameTime(Integer.valueOf(gameField4.getText()));
+                    break;
+                } 
+                catch(NumberFormatException i){
+                    System.out.println("Invalid Hours Number. Try Again!");
+                }
+            }
+            gameField4.setText("");
+            gameList.add(newGame);
+
+            
+        });
+
+
+        inputPanel.add(gameLabel1);
+        inputPanel.add(gameField1);
+        inputPanel.add(gameLabel2);
+        inputPanel.add(gameField2);
+        inputPanel.add(gameLabel3);
+        inputPanel.add(gameField3);
+        inputPanel.add(gameLabel4);
+        inputPanel.add(gameField4);
+
+        inputPanel.add(Box.createVerticalStrut(5));
+        inputPanel.add(submitButton);
+
+
+        uiWindow.add(homePanel, BorderLayout.NORTH);
+        uiWindow.add(inputPanel, BorderLayout.CENTER);
+        uiWindow.setVisible(true);
     }
+
+    public static JPanel createHomePanel(ArrayList<Games> gameList, JFrame previous){
+
+        JPanel homePanel = new JPanel();
+
+        homePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        homePanel.setBackground(Color.darkGray);
+
+        homePanel.add(Box.createRigidArea(new DimensionUIResource(1, 20)));
+
+        JButton homeButton = new JButton("Home");
+        homeButton.addActionListener(e -> mainMenu(gameList, previous));
+        homeButton.addActionListener(e-> previous.dispose());
+
+        JButton newGameButton = new JButton("Add New Games");
+        newGameButton.addActionListener(e -> addGamesMenu(gameList, previous));
+        newGameButton.addActionListener(e-> previous.dispose());
+
+        homePanel.add(homeButton);
+        homePanel.add(newGameButton);
+
+        return homePanel;
+    }
+
+    public static void populateCSV(Games g){
+
+        BufferedWriter w = null;
+
+        try {
+            w = new BufferedWriter(new FileWriter("/src/Data/gameList.csv"));
+            w.write(g.getName() + ", " + g.getFilePath() + ", " + g.getIconPath() + ", " + g.getGameTime() + ", ");
+            w.close();
+        }
+        catch (IOException e){
+            System.err.println("File Write Error");
+            System.exit(7);
+        }
+        
+    }
+
 }
