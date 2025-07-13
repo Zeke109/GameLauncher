c7c1e94903acca95b6b6435a13559972ccff1381
Zeke109 <zachyvernon@gmail.com>
2025-07-13 11:47:34 -0400

    Added csv reading functionality

--- gameLauncher/src/App.java
+++ gameLauncher/src/App.java
@@ -1,73 +1,157 @@
 import First.Games;
 import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.plaf.DimensionUIResource;
 import java.awt.Image;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Desktop;
+import java.awt.FlowLayout;
+
 
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         FileInputStream data = null; 
-        try {
-            data = new FileInputStream("src/Data/gameList.csv");
+        ArrayList<Games> gameList = new ArrayList<Games>();
+
+        while (true){
+            try {
+                File file = new File("src/Data/gameList.csv"); 
+                if (file.createNewFile())
+
+                if (file.length() == 0){
+                    addGames();
+                }
+
+                data = new FileInputStream("src/Data/gameList.csv");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(data));
+
+                String line;
+                boolean isFirstLine = true;
+
+                while ((line = reader.readLine()) != null){
+
+                    if (isFirstLine)
+                        isFirstLine = false;
+
+                    String[] part = line.split(",");
+                    ArrayList<String> parts = new ArrayList<String>();
+                    for (int i = 0; i < part.length; i++){
+                        parts.add(part[i]);
+                    }
+
+                    if (parts.size() % 4 == 0){
+                        while (parts.size() > 0){
+                            Games newGame = new Games();
+                            newGame.setName(parts.get(0));
+                            newGame.setFilePath(parts.get(1));
+                            newGame.setIconPath(parts.get(2));
+                            try {
+                                newGame.setGameTime(Integer.valueOf(parts.get(3)));
+                            } 
+                            catch (NumberFormatException e)
+                            {
+                                System.err.println("Invalid gameTime format");
+                                System.exit(4);
+                            }
+
+                            gameList.add(newGame);
+                            for (int i = 0; i < 4; i++){
+                                parts.remove(0);
+                            }
+                        }
+                    }
+                    else{
+                        System.err.println("Invalid csv format");
+                        System.exit(4);
+                    }
+                }
+                reader.close();
+                break;
+            }
+
+            catch (FileNotFoundException e){
+                System.err.println("File not found. Creating new Gamelist file");
+                File file = new File("src/Data/gameList.csv");
+                if (!(file.createNewFile())){
+                    System.err.println("File Creation Unsuccessfull");
+                    System.exit(4);
+                }
+            }
+
+            catch (IOException e){
+                
+                System.err.println("File Read Error");
+                System.exit(1);
+            }
         }
-        catch (IOException e){
-            System.err.println("File Read Error");
-            System.exit(1);
-        }
+
+        mainMenu(gameList);
         
 
-        //while (true) {
+        data.close();
+    }
 
-            //Games game = new Games("temp", "temp", "temp", 0);
-            Games newGame = new Games();
-            newGame.setFilePath("C:/Users/Zach/AppData/Local/Discord/app-1.0.9199/Discord.exe");
+    public static void mainMenu(ArrayList<Games> gameList){
+        JFrame uiWindow = new JFrame("Zeke's Game Launcher");
+        uiWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        uiWindow.setSize(950, 540);
 
-            newGame.setGameTime(0);
+        JPanel homePanel = new JPanel();
+        homePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        homePanel.setBackground(Color.darkGray);
+
+        homePanel.add(Box.createRigidArea(new DimensionUIResource(10, 20)));
+
+        homePanel.add(new JButton("Add New Games"));
+
+
+        JPanel gamesPanel = new JPanel();
+        gamesPanel.setLayout(new BoxLayout(gamesPanel, BoxLayout.Y_AXIS));
+        
+        gamesPanel.setBackground(new Color(000051204)); // Blue 
+
+
+        for (int i = 0; i < gameList.size(); i++){
             
-            JFrame uiWindow = new JFrame("Launcher");
-            uiWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            uiWindow.setSize(950, 540);
+            ImageIcon gameLogo = new ImageIcon(gameList.get(i).getIconPath());
+            Image scaledImage = gameLogo.getImage().getScaledInstance(125, 125, Image.SCALE_SMOOTH);
+            ImageIcon scaledIcon = new ImageIcon(scaledImage);
+            JButton icon = new JButton(gameList.get(i).getName(), scaledIcon);
 
-            JPanel panel = new JPanel();
-            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            System.out.println("Path: " + gameList.get(i).getIconPath());
+            System.out.println("File exists: " + new File(gameList.get(i).getIconPath()).exists());
+
+            icon.setPreferredSize(new DimensionUIResource(125, 125));
+            icon.setMaximumSize(new DimensionUIResource(125, 125));
+
+            icon.setHorizontalTextPosition(SwingConstants.CENTER);
+            icon.setVerticalTextPosition(SwingConstants.BOTTOM);
+
+            File file = new File(gameList.get(i).getFilePath());
+            icon.addActionListener(e -> runProgram(file));
+
+            gamesPanel.add(Box.createRigidArea(new DimensionUIResource(10, 20)));
+            gamesPanel.add(icon);
             
+        }
+        
+        gamesPanel.revalidate();
+        gamesPanel.repaint();
 
-            //for (int i = 0; i < 100; i++){
-                
-                ImageIcon gameLogo = new ImageIcon("src/images/game.png");
-                Image scaledImage = gameLogo.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
-                ImageIcon gameLogo2 = new ImageIcon(scaledImage);
-                JButton temp = new JButton(gameLogo2);
-                
-                
-                temp.setPreferredSize(new DimensionUIResource(100, 100));
-                temp.setMaximumSize(new DimensionUIResource(100, 100));
-                File file = new File(newGame.getFilePath());
-                
-                
+        JScrollPane scroll = new JScrollPane(gamesPanel);
+        scroll.getVerticalScrollBar().setUnitIncrement(20);
 
-                temp.addActionListener(e -> runProgram(file));
-
-                panel.add(Box.createRigidArea(new DimensionUIResource(5, 20)));
-                panel.add(temp);
-            //}
-            
-            JScrollPane scroll = new JScrollPane(panel);
-            scroll.getVerticalScrollBar().setUnitIncrement(20);
-
-            uiWindow.add(scroll);
-            uiWindow.setVisible(true);
-
-            data.close();
+        uiWindow.setLayout(new BorderLayout());
+        uiWindow.add(homePanel, BorderLayout.NORTH);
+        uiWindow.add(scroll, BorderLayout.CENTER);
+        uiWindow.setVisible(true);
     }
             
-
-        //}
-
-        
 
     public static void runProgram (File file){
         Desktop dt = Desktop.getDesktop();
@@ -76,7 +160,11 @@ public class App {
         }
         catch (IOException e){
             System.err.println("Cannot find requested executable");
-            System.exit(4);
+            return; 
         }
+    }
+
+    public static void addGames() {
+        System.out.println("Success");
     }
 }
