47db689bc1ed9bb1cf78d02bb21a4d56de3d9ec5
Zeke109 <zachyvernon@gmail.com>
2025-07-15 12:50:27 -0400

    Added removal capabilites, file selection and images

--- gameLauncher/src/App.java
+++ gameLauncher/src/App.java
@@ -2,7 +2,6 @@ import First.Games;
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
-
 import javax.sound.midi.SysexMessage;
 import javax.swing.*;
 import javax.swing.plaf.DimensionUIResource;
@@ -10,6 +9,7 @@ import java.awt.Image;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Desktop;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 
@@ -40,10 +40,14 @@ public class App {
                     if (isFirstLine)
                         isFirstLine = false;
 
-                    String[] part = line.split(", ");
+                    String[] part = line.split("; ");
                     ArrayList<String> parts = new ArrayList<String>();
                     for (int i = 0; i < part.length; i++){
                         parts.add(part[i]);
+                    }
+
+                    for (int i = 0; i < parts.size(); i++){
+                        System.out.println(parts.get(i));
                     }
 
                     if (parts.size() % 4 == 0){
@@ -94,9 +98,9 @@ public class App {
 
 
         mainMenu(gameList, null);
-        
 
         data.close();
+
     }
 
     public static void mainMenu(ArrayList<Games> gameList, JFrame previous){
@@ -111,10 +115,13 @@ public class App {
         
         gamesPanel.setBackground(new Color(000051204)); // Blue 
 
+        gamesPanel.revalidate();
+        gamesPanel.repaint();
+
         for (int i = 0; i < gameList.size(); i++){
             
             ImageIcon gameLogo = new ImageIcon(gameList.get(i).getIconPath());
-            Image scaledImage = gameLogo.getImage().getScaledInstance(125, 125, Image.SCALE_SMOOTH);
+            Image scaledImage = gameLogo.getImage().getScaledInstance(125, 100, Image.SCALE_SMOOTH);
             ImageIcon scaledIcon = new ImageIcon(scaledImage);
             JButton icon = new JButton(gameList.get(i).getName(), scaledIcon);
 
@@ -132,8 +139,6 @@ public class App {
             
         }
         
-        gamesPanel.revalidate();
-        gamesPanel.repaint();
 
         JScrollPane scroll = new JScrollPane(gamesPanel);
         scroll.getVerticalScrollBar().setUnitIncrement(20);
@@ -164,6 +169,7 @@ public class App {
         JFrame uiWindow = new JFrame("Zeke's Game Launcher");
         uiWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         uiWindow.setSize(950, 540);
+        uiWindow.setBackground(Color.gray);
 
         JPanel homePanel = createHomePanel(gameList, uiWindow);
 
@@ -171,16 +177,49 @@ public class App {
         inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
 
         JLabel gameLabel1 = new JLabel("\nEnter the game's name: ");
-        JTextField gameField1 = new JTextField(5); 
+        JTextField gameField1 = new JTextField(1); 
+        gameField1.setPreferredSize(new Dimension(1500, 30));
+        gameField1.setMaximumSize(new Dimension(1500, 30));
+        gameField1.setAlignmentX(-100);
 
         JLabel gameLabel2 = new JLabel("\nEnter the game's path");
-        JTextField gameField2 = new JTextField(1); 
+        JButton gpathButton = new JButton("Enter File Path"); 
+
+        
+        final String[] gamePath = new String[1];
+        final String[] imagePath = new String[1];
+        gamePath[0] = "0";
+        imagePath[0] = "0";
+
+        gpathButton.addActionListener(e -> {
+            JFileChooser fc = new JFileChooser();
+            int res = fc.showOpenDialog(uiWindow);
+        
+            if (res == JFileChooser.APPROVE_OPTION) {
+                File selectFile = fc.getSelectedFile();
+                gamePath[0] = selectFile.getAbsolutePath();
+            }
+        });
 
         JLabel gameLabel3 = new JLabel("\nEnter the path for the game icon");
-        JTextField gameField3 = new JTextField(1); 
+        JButton ipathButton = new JButton("Enter File Path");
+        
+        ipathButton.addActionListener(e -> {
+            JFileChooser fc = new JFileChooser();
+            int res = fc.showOpenDialog(uiWindow);
+        
+            if (res == JFileChooser.APPROVE_OPTION) {
+                File selectFile = fc.getSelectedFile();
+                imagePath[0] = selectFile.getAbsolutePath();
+            }
+        });
 
         JLabel gameLabel4 = new JLabel("\nEnter how many hours you have ");
         JTextField gameField4 = new JTextField(1); 
+
+        gameField4.setPreferredSize(new Dimension(60, 30));
+        gameField4.setMaximumSize(new Dimension(60, 30));
+        gameField4.setAlignmentX(-100);
 
         JButton submitButton = new JButton("\nAdd Game");
 
@@ -188,18 +227,13 @@ public class App {
             Games newGame = new Games();
             newGame.setName(gameField1.getText());
             gameField1.setText("");
-            newGame.setFilePath(gameField2.getText());
-            gameField2.setText("");
-            newGame.setIconPath(gameField3.getText());
-            gameField3.setText("");
-            while (true){
-                try{
-                    newGame.setGameTime(Integer.valueOf(gameField4.getText()));
-                    break;
-                } 
-                catch(NumberFormatException i){
-                    System.out.println("Invalid Hours Number. Try Again!");
-                }
+            newGame.setFilePath(gamePath[0]);
+            newGame.setIconPath(imagePath[0]);
+            try{
+                newGame.setGameTime(Integer.valueOf(gameField4.getText()));
+            } 
+            catch(NumberFormatException i){
+                System.out.println("Invalid Hours Number. Try Again!");
             }
             gameField4.setText("");
             gameList.add(newGame);
@@ -207,13 +241,12 @@ public class App {
             populateCSV(newGame);
         });
 
-
         inputPanel.add(gameLabel1);
         inputPanel.add(gameField1);
         inputPanel.add(gameLabel2);
-        inputPanel.add(gameField2);
+        inputPanel.add(gpathButton);
         inputPanel.add(gameLabel3);
-        inputPanel.add(gameField3);
+        inputPanel.add(ipathButton);
         inputPanel.add(gameLabel4);
         inputPanel.add(gameField4);
 
@@ -243,10 +276,35 @@ public class App {
         newGameButton.addActionListener(e -> addGamesMenu(gameList, previous));
         newGameButton.addActionListener(e-> previous.dispose());
 
+        JButton removeButton = new JButton("Remove Game");
+        removeButton.addActionListener(e -> removeGameMenu(gameList, previous)); 
+        removeButton.addActionListener(e -> previous.dispose());
+
         homePanel.add(homeButton);
         homePanel.add(newGameButton);
+        homePanel.add(removeButton);
 
         return homePanel;
+    }
+
+    public static void cleaner(ArrayList<Games> gameList){
+
+        BufferedWriter csvFile = null;
+             
+        try{
+            csvFile = new BufferedWriter(new FileWriter("src/Data/gameList.csv"));
+            csvFile.write("");
+            csvFile = new BufferedWriter(new FileWriter("src/Data/gameList.csv", true));
+    
+            for (int i = 0; i < gameList.size(); i++){
+                populateCSV(gameList.get(i));
+            }
+        }
+        catch (IOException e){
+            System.err.println("Failed to update CSV file. Additions or deletions may not have been properly saved");
+            System.exit(9);
+        }
+    
     }
 
     public static void populateCSV(Games g){
@@ -255,7 +313,7 @@ public class App {
 
         try {
             w = new BufferedWriter(new FileWriter("src/Data/gameList.csv", true));
-            w.write(g.getName() + ", " + g.getFilePath() + ", " + g.getIconPath() + ", " + g.getGameTime() + ", ");
+            w.write(g.getName() + "; " + g.getFilePath() + "; " + g.getIconPath() + "; " + g.getGameTime() + "\n");
             w.close();
         }
         catch (IOException e){
@@ -265,4 +323,63 @@ public class App {
         
     }
 
+    public static void removeLoop (ArrayList<Games> gameList, JPanel inputPanel){
+
+        for (int j = 0; j < gameList.size(); j++){
+    
+            ImageIcon gameLogo = new ImageIcon(gameList.get(j).getIconPath());
+            Image scaledImage = gameLogo.getImage().getScaledInstance(125, 100, Image.SCALE_SMOOTH);
+            ImageIcon scaledIcon = new ImageIcon(scaledImage);
+            JButton icon = new JButton(gameList.get(j).getName(), scaledIcon);
+    
+            icon.setPreferredSize(new DimensionUIResource(125, 125));
+            icon.setMaximumSize(new DimensionUIResource(125, 125));
+    
+            icon.setHorizontalTextPosition(SwingConstants.CENTER);
+            icon.setVerticalTextPosition(SwingConstants.BOTTOM);
+    
+            int index = j;
+            icon.addActionListener(e -> 
+            {
+                gameList.remove(index);
+                cleaner(gameList);
+                icon.setVisible(false);
+            });
+            
+            inputPanel.add(Box.createRigidArea(new DimensionUIResource(10, 20)));
+            inputPanel.add(icon);
+        }
+
+        
+    
+    }
+
+    public static void removeGameMenu (ArrayList<Games> gameList, JFrame previous){
+
+        JFrame uiWindow = new JFrame("Zeke's Game Launcher");
+        uiWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        uiWindow.setSize(950, 540);
+
+
+        JPanel homePanel = createHomePanel(gameList, uiWindow);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+        inputPanel.setBackground(new Color(234057085));
+
+        inputPanel.revalidate();
+
+        removeLoop(gameList, inputPanel);
+
+        inputPanel.revalidate();
+
+        uiWindow.add(homePanel, BorderLayout.NORTH);
+        uiWindow.add(inputPanel, BorderLayout.CENTER);
+        uiWindow.setVisible(true);
+    }
+
 }
+
+
+
+
